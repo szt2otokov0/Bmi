af1de9eda6d289bf582b456945f25a86bb1093bc
szt2otokov <szt2otokov@vasvari.org>
2023-09-14 13:01:46 +0200

    dun

--- src/main.java
+++ src/main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
 
+        double bmi = massInKg/Math.pow(heightInM,2);
+        System.out.println("Your BIM is: " + bmi);
     }
 }
